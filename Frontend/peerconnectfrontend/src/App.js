96f593590c4aba339c570391df617d52e92fe97c
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-09-18 23:46:58 +0530

    Added UserProfile

--- Frontend/peerconnectfrontend/src/App.js
+++ Frontend/peerconnectfrontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./Components/Login";
 import UserCard from "./Components/UserCard";
+import UserProfile from "./Components/UserProfile";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/user" component={UserCard} />
+        <Route path="/profile" component={UserProfile} />
       </Switch>
     </Router>
   );
